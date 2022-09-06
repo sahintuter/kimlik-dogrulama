cdb97729f9c217b44ce94c513f5e9bed5fc2e580
Şahin TÜTER <71895937+sahintuter@users.noreply.github.com>
2022-09-06 13:20:30 +0300

    Add files via upload

---
+++ KimlikDogrulama/KimlikDogrulama/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace KimlikDogrulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxSurname = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sorgula = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(268, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TCKN: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(293, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(263, 454);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 28);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Soyad: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(196, 537);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 28);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Doğum Tarihi: ";
+            // 
+            // tbxName
+            // 
+            this.tbxName.Font = new System.Drawing.Font("Yu Gothic", 8F, System.Drawing.FontStyle.Bold);
+            this.tbxName.Location = new System.Drawing.Point(360, 360);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(217, 33);
+            this.tbxName.TabIndex = 2;
+            // 
+            // tbxSurname
+            // 
+            this.tbxSurname.Font = new System.Drawing.Font("Yu Gothic", 8F, System.Drawing.FontStyle.Bold);
+            this.tbxSurname.Location = new System.Drawing.Point(360, 449);
+            this.tbxSurname.Name = "tbxSurname";
+            this.tbxSurname.Size = new System.Drawing.Size(217, 33);
+            this.tbxSurname.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // date
+            // 
+            this.date.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.date.Location = new System.Drawing.Point(360, 537);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(217, 31);
+            this.date.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(331, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 36);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "TCKN Doğrulama";
+            // 
+            // sorgula
+            // 
+            this.sorgula.Location = new System.Drawing.Point(377, 620);
+            this.sorgula.Name = "sorgula";
+            this.sorgula.Size = new System.Drawing.Size(171, 44);
+            this.sorgula.TabIndex = 6;
+            this.sorgula.Text = "Sorgula";
+            this.sorgula.UseVisualStyleBackColor = true;
+            this.sorgula.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Yu Gothic", 8F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(360, 276);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(917, 691);
+            this.Controls.Add(this.sorgula);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbxSurname);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Kimlik Doğrulama";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private static System.Windows.Forms.TextBox tbxTckn;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxSurname;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button sorgula;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ KimlikDogrulama/KimlikDogrulama/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KimlikDogrulama.Dogrulama;
+using  MetroFramework;
+
+namespace KimlikDogrulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string ad, soyad;
+            long tckn = long.Parse(textBox1.Text);
+            int dogumTarihi;
+            ad = tbxName.Text;
+            soyad = tbxSurname.Text;
+            dogumTarihi = date.Value.Year;
+            Dogrulama.KPSPublicSoapClient dogrulama = new KPSPublicSoapClient();
+            bool sonuc = dogrulama.TCKimlikNoDogrula(tckn, ad, soyad, dogumTarihi);
+
+            if (sonuc == true)
+            {
+                MessageBox.Show("Bilgiler Doğru", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                
+            }
+
+            if (sonuc==false)
+            {
+                
+                MessageBox.Show("Bilgiler Yanlış", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+               
+            }
+        }
+
+       
+    }
+}
